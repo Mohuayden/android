9b729a6c0b22910fdcc495446c371399508a25c9
Mohuayden <sarunpong.mm@gmail.com>
2023-10-05 16:49:08 +0700

    Revert "Edit1"
    This reverts commit a3220ee6533bcca92ef7405c5c1bc0658756eb93.

--- example-app/app/src/callback/java/com.iproov.example/MainActivityCallback.kt
+++ example-app/app/src/callback/java/com.iproov.example/MainActivityCallback.kt
@@ -8,7 +8,6 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.fuel.json.jsonDeserializer
-import com.github.kittinunf.fuel.toolbox.HttpClient
 import com.iproov.androidapiclient.AssuranceType
 import com.iproov.androidapiclient.ClaimType
 import com.iproov.androidapiclient.DemonstrationPurposesOnly
@@ -22,8 +21,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-import kotlin.random.Random
-
 
 class MainActivityCallback : AppCompatActivity() {
 
@@ -31,8 +28,7 @@ class MainActivityCallback : AppCompatActivity() {
     private val uiScope = CoroutineScope(Dispatchers.Main + job)
     private lateinit var binding: ActivityMainBinding
     private val iProovCallbackLauncher: IProovCallbackLauncher = IProovCallbackLauncher()
-    private var userId = ""
-    private var token = ""
+
     private val listener = object : IProovCallbackLauncher.Listener {
 
         override fun onConnecting() {
@@ -43,66 +39,11 @@ class MainActivityCallback : AppCompatActivity() {
             binding.progressBar.isIndeterminate = false
         }
 
-        override fun onSuccess(result: IProov.SuccessResult) {
-            //onResult(getString(R.string.success), "")
-            println(result)
-            val apiClientFuel = ApiClientFuel(
-                this@MainActivityCallback,
-                Constants.FUEL_URL,
-                Constants.API_KEY,
-                Constants.SECRET
-            )
-//            val apiClientFuel = ApiClientFuel(
-//                this,
-//                Constants.FUEL_URL,
-//                Constants.API_KEY,
-//                Constants.SECRET
-//            )
-//            uiScope.launch(Dispatchers.IO) {
-//                try {
-//                    val token = apiClientFuel.getToken(
-//                        assuranceType,
-//                        claimType,
-//                        username
-//                    )
-//                    println(token)
-//
-//                    try {
-////                        iProovCallbackLauncher.launch(
-////                            this@MainActivityCallback,
-////                            Constants.BASE_URL,
-////                            token
-////                        )
-//                        onResult(getString(R.string.success), "")
-//                    } catch (ex: Exception) {
-//                        withContext(Dispatchers.Main) {
-//                            onResult(getString(R.string.error), ex.localizedMessage)
-//                        }
-//                    }
-//                } catch (ex: Exception) {
-//                    withContext(Dispatchers.Main) {
-//                        ex.printStackTrace()
-//                        if (ex is FuelError) {
-//                            val json = jsonDeserializer().deserialize(ex.response)
-//                            val description = json.obj().getString("error_description")
-//                            onResult(getString(R.string.error), description)
-//                        } else {
-//                            onResult(getString(R.string.error), getString(R.string.failed_to_get_token))
-//                        }
-//                    }
-//                }
-//            }
-        }
+        override fun onSuccess(result: IProov.SuccessResult) =
+            onResult(getString(R.string.success), "")
 
-
-        override fun onFailure(result: IProov.FailureResult) {
-            println(result.reason)
-            println(userId)
+        override fun onFailure(result: IProov.FailureResult) =
             onResult(result.reason.feedbackCode, getString(result.reason.description))
-            // val randomString = generateRandomString()
-            //  launchIProov(ClaimType.ENROL, randomString,  AssuranceType.GENUINE_PRESENCE)
-        }
-
 
         override fun onProcessing(progress: Double, message: String?) {
             binding.progressBar.progress = progress.times(100).toInt()
@@ -125,22 +66,11 @@ class MainActivityCallback : AppCompatActivity() {
             throw IllegalStateException("You must set the API_KEY and SECRET values in the Constants.kt file!")
         }
 
-        arrayOf(
-            binding.enrolGpaButton,
-            binding.verifyLaButton,
-            binding.verifyGpaButton
-        ).forEach { it ->
+        arrayOf(binding.enrolGpaButton, binding.verifyLaButton, binding.verifyGpaButton).forEach { it ->
             it.setOnClickListener {
-                //val username = binding.usernameEditText.text.toString()
-                userId = binding.usernameEditText.text.toString()
-               //  userId = generateRandomString()
-
-                if (userId.isEmpty()) {
-                    Toast.makeText(
-                        this,
-                        getString(R.string.user_id_cannot_be_empty),
-                        Toast.LENGTH_SHORT
-                    ).show()
+                val username = binding.usernameEditText.text.toString()
+                if (username.isEmpty()) {
+                    Toast.makeText(this, getString(R.string.user_id_cannot_be_empty), Toast.LENGTH_SHORT).show()
                 } else {
                     val claimType = when (it) {
                         binding.enrolGpaButton -> ClaimType.ENROL
@@ -155,40 +85,14 @@ class MainActivityCallback : AppCompatActivity() {
                         binding.verifyLaButton -> AssuranceType.LIVENESS
                         else -> throw NotImplementedError()
                     }
-                    println(claimType)
-                    println(userId)
-                    println(assuranceType)
-                    launchIProov(claimType, userId, assuranceType)
+                    launchIProov(claimType, username, assuranceType)
                 }
             }
         }
 
-        binding.versionTextView.text =
-            getString(R.string.kotlin_version_format, iProovCallbackLauncher.sdkVersion)
+        binding.versionTextView.text = getString(R.string.kotlin_version_format, iProovCallbackLauncher.sdkVersion)
 
         iProovCallbackLauncher.listener = listener
-    }
-
-    fun generateRandomString(): String {
-        val chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        val chars2 = "abcdefghijklmnopqrstuvwxyz"
-        val random = Random.Default
-        val mailBuilder = StringBuilder()
-
-
-        for (i in 1..10) {
-            val randomIndex = random.nextInt(chars.length)
-            val randomChar = chars[randomIndex]
-            mailBuilder.append(randomChar)
-        }
-        mailBuilder.append("@")
-        for (i in 1..5) {
-            val randomIndex2 = random.nextInt(chars2.length)
-            val randomChar2 = chars2[randomIndex2]
-            mailBuilder.append(randomChar2)
-        }
-        mailBuilder.append(".com")
-        return mailBuilder.toString()
     }
 
     override fun onDestroy() {
@@ -210,19 +114,14 @@ class MainActivityCallback : AppCompatActivity() {
 
         uiScope.launch(Dispatchers.IO) {
             try {
-               token = apiClientFuel.getToken(
+                val token = apiClientFuel.getToken(
                     assuranceType,
                     claimType,
                     username
                 )
-                println(token)
 
                 try {
-                    iProovCallbackLauncher.launch(
-                        this@MainActivityCallback,
-                        Constants.BASE_URL,
-                        token
-                    )
+                    iProovCallbackLauncher.launch(this@MainActivityCallback, Constants.BASE_URL, token)
                 } catch (ex: Exception) {
                     withContext(Dispatchers.Main) {
                         onResult(getString(R.string.error), ex.localizedMessage)
@@ -250,61 +149,7 @@ class MainActivityCallback : AppCompatActivity() {
         AlertDialog.Builder(this@MainActivityCallback)
             .setTitle(title)
             .setMessage(resultMessage)
-            .setPositiveButton("Try again") { dialog, _ ->
-                val randomString = generateRandomString()
-                launchIProov(ClaimType.ENROL, randomString, AssuranceType.GENUINE_PRESENCE)
-            }
-            .setNegativeButton("cancle") { dialog, _ -> dialog.cancel() }
+            .setPositiveButton(android.R.string.ok) { dialog, _ -> dialog.cancel() }
             .show()
     }
-
-
-//    fun makeHttpPostRequest() {
-//        // Define the URL for your API endpoint
-//        val apiUrl = "https://api.example.com/post_endpoint"
-//
-//        // Create an HttpClient
-//        val httpClient: HttpClient = DefaultHttpClient()
-//
-//        // Create an HttpPost object
-//        val httpPost = HttpPost(apiUrl)
-//
-//        // Define the JSON payload for your POST request
-//        val jsonPayload = "{\"key1\": \"value1\", \"key2\": \"value2\"}"
-//
-//        // Set the JSON payload as the request entity
-//        val entity = StringEntity(jsonPayload, "UTF-8")
-//        entity.setContentType("application/json")
-//        httpPost.entity = entity
-//
-//        try {
-//            // Execute the POST request
-//            val response: HttpResponse = httpClient.execute(httpPost)
-//
-//            // Get the response entity
-//            val inputStream = response.entity.content
-//
-//            // Read the response content
-//            val reader = BufferedReader(InputStreamReader(inputStream))
-//            val stringBuilder = StringBuilder()
-//            var line: String?
-//
-//            while (reader.readLine().also { line = it } != null) {
-//                stringBuilder.append(line).append("\n")
-//            }
-//
-//            val responseBody = stringBuilder.toString()
-//
-//            // Handle the response data as needed
-//            println("Response: $responseBody")
-//
-//            // Close the input stream and release resources
-//            inputStream.close()
-//        } catch (e: Exception) {
-//            e.printStackTrace()
-//        }
-//    }
-
-
-
 }
--- example-app/app/src/main/java/com.iproov.example/Constants.kt
+++ example-app/app/src/main/java/com.iproov.example/Constants.kt
@@ -1,10 +1,9 @@
 // Copyright (c) 2020 iProov Ltd. All rights reserved.
 package com.iproov.example
 
-object Constants {;
-    const val BASE_URL = "wss://sg.rp.secure.iproov.me/ws"
-    const val FUEL_URL = "https://sg.rp.secure.iproov.me/api/v2/"
-    const val API_KEY = "5378cbb3ecf39e2eef290298a04cd006de76cc9a" // TODO: Add your API key here
-    const val SECRET = "f664534a523e850eee56d971d6ee89f7a572cdcb" // TODO: Add your Secret here
-    const val USER_NAME = "dusitm@lhbank.co.th"
+object Constants {
+    const val BASE_URL = "wss://beta.rp.secure.iproov.me/ws"
+    const val FUEL_URL = "https://beta.rp.secure.iproov.me/api/v2/"
+    const val API_KEY = "" // TODO: Add your API key here
+    const val SECRET = "" // TODO: Add your Secret here
 }
